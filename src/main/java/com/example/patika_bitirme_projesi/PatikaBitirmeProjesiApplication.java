7f4a3dc910009caaaa0fbb8b09be4d03934f5798
Ahmet Sefa Altundal <58665552+Sefahmet@users.noreply.github.com>
2022-01-20 20:14:02 +0300

    Initializing The Fİnal Project

---
+++ src/main/java/com/example/patika_bitirme_projesi/PatikaBitirmeProjesiApplication.java
@@ -0,0 +1,14 @@
+package com.example.patika_bitirme_projesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaBitirmeProjesiApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PatikaBitirmeProjesiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/patika_bitirme_projesi/PatikaBitirmeProjesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patika_bitirme_projesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaBitirmeProjesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
